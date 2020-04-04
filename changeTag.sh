0cb34d0685e12301ddb5494d68a42b3fdfbe5280
soufiane makhloufi <45273948+soufianem370@users.noreply.github.com>
2020-04-04 02:43:34 +0100

    Update changeTag.sh

--- changeTag.sh
+++ changeTag.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 #sed "s/tagVersion/$1/g" myweb.yaml > myweb.yaml
-printf "%s\n" '1,$s/tagVersion/${BUILD_NUMBER}/g' wq | ed -s myweb.yaml
+sed -i 's/tagVersion/${BUILD_NUMBER}/g' myweb.yaml
 
